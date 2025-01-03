586867f3a3d806142dbbc05e80df746437fc9423
Alishba ishaq <sanaishaq934@gmail.com>
2025-01-04 00:07:45 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/hero";
+
+const Homepage = () =>{
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+export default Homepage 
